7a40bc5157721282ba738646b878eb77f5531e38
Handher0 <handher0@naver.com>
2024-07-03 01:43:57 +0900

    JavaScript-displayitems

--- Javascript_쇼핑몰_미니게임_과제/main.js
+++ Javascript_쇼핑몰_미니게임_과제/main.js
@@ -1,13 +1,24 @@
 function loadItems(){
     return fetch('data.json')
     .then(response => response.json())
-    .then(json => console.log(json.items));
+    .then(json => json.items);
 }
-
+function displayItems(items){
+    container=document.querySelector('.items');
+    container.innerHTML=items.map(item=>createHTMLString(item)).join('');
+}
+function createHTMLString(item){
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
 loadItems()
 .then(items => {
-    console.log(items);
-    // displayItems(items);
+    //console.log(items);
+    displayItems(items);
     // setEventListeners(items)
 })
 .catch(console.log);
