434e4772f1bcedf218a25ef6f53cf9f76b0a3809
Handher0 <handher0@naver.com>
2024-07-05 17:56:11 +0900

    python_problem1_step8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,40 +1,30 @@
+def brGame(player, num):
+    while True:
+        try:
+            cnt = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            if cnt == 1 or cnt == 2 or cnt == 3 :
+                break
+            else :
+                print("1, 2, 3 중 하나를 입력하세요")
+        except ValueError:
+            print("정수를 입력하세요")
+
+    for i in range(0, cnt):  
+        num += 1
+        print(f"{player} : {num}")
+        if num >= 31:  
+            return num
+    return num
+
+
 num = 0
 
 while True:
-    while True:
-        try:
-            cnt = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-            if cnt == 1 or cnt == 2 or cnt == 3 :
-                break
-            else :
-                print("1, 2, 3 중 하나를 입력하세요")
-        except ValueError:
-            print("정수를 입력하세요")
-
-    for i in range(0, cnt):  
-        num += 1
-        print(f"playerA : {num}")
-        if num >= 31:
-            print("player B win!")    
-            break
-    if num >= 31:
+    num = brGame("playerA", num)
+    if num == 31:
+        print("playerB is win")
         break
-
-    while True:
-        try:
-            cnt = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-            if cnt == 1 or cnt == 2 or cnt == 3 :
-                break
-            else :
-                print("1, 2, 3 중 하나를 입력하세요")
-        except ValueError:
-            print("정수를 입력하세요")
-
-    for i in range(0, cnt):  
-        num += 1
-        print(f"playerB : {num}")
-        if num >= 31:
-            print("player B win!")    
-            break
-    if num >= 31:
+    num = brGame("playerB", num)
+    if num == 31:
+        print("playerA is win")
         break
