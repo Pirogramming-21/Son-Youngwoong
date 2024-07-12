65051f2a79a605b52b61776790ad80dc36dfbeb3
Handher0 <handher0@naver.com>
2024-07-13 01:38:27 +0900

    기본 과제 완성

--- myMovieReviews/reviews/models.py
+++ myMovieReviews/reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class MovieReview(models.Model):
+    title = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    main_cast = models.CharField(max_length=200)
+    genre = models.CharField(max_length=50)
+    release_year = models.IntegerField()
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    running_time = models.IntegerField()
+    review_text = models.TextField()
+
+    def __str__(self):
+        return self.title
--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import MovieReview
+from .forms import MovieReviewForm
 
-# Create your views here.
+def review_list(request):
+    reviews = MovieReview.objects.all()
+    return render(request, 'reviews/review_list.html', {'reviews': reviews})
 
-def reiview_list(request):
-    return render(request, 'review_list.html', {})
+def review_detail(request, pk):
+    review = get_object_or_404(MovieReview, pk=pk)
+    return render(request, 'reviews/review_detail.html', {'review': review})
+
+def review_create(request):
+    if request.method == "POST":
+        form = MovieReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save()
+            return redirect('review_detail', pk=review.pk)
+    else:
+        form = MovieReviewForm()
+    return render(request, 'reviews/review_form.html', {'form': form})
+
+def review_update(request, pk):
+    review = get_object_or_404(MovieReview, pk=pk)
+    if request.method == "POST":
+        form = MovieReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            review = form.save()
+            return redirect('review_detail', pk=review.pk)
+    else:
+        form = MovieReviewForm(instance=review)
+    return render(request, 'reviews/review_form.html', {'form': form})
+
+def review_delete(request, pk):
+    review = get_object_or_404(MovieReview, pk=pk)
+    review.delete()
+    return redirect('review_list')
